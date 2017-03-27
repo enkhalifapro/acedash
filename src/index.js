920b6e2fd38db642e7dc84c543eccca373bf849e
ayman <enkhalifapro@gmail.com>
2017-03-27 20:51:07 +0200

    add assets dir

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducers from './reducers';
 import BookList from'./containers/book_list';
 
 class App extends Component {
-	render()
-	{
-		return (
-			<BookList/>
-		);
-	}
+    render() {
+        return (
+            <BookList/>
+        );
+    }
 }
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 // put it in dom
 ReactDom.render(<Provider store={createStoreWithMiddleware(reducers)}>
-	<App/>
+    <App/>
 </Provider>, document.querySelector('.container'));
